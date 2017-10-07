7bb143d592f196d7ce83e66d37f1f0af00da38e1
Dawid Dworak <dawiddworak88@gmail.com>
2017-10-07 16:33:30 +0200

    L-10 Replaced XHR AJAX with JQuery AJAX

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -31,35 +31,32 @@ function GetExistingComments() {
 }
 
 function ExistingCommentsAjaxQuery(url) {
-    var xhr = new XMLHttpRequest();
+
+    $.ajax({
+        type: "GET",
+        url: getCommentsUrl() + "?link=" + url,
+        success: function(comments) {
+
+            if (comments) {
+                var html = '';
     
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState != 4) {
-            return;
-        }
-
-        if (this.responseText) {
-            var comments = JSON.parse(this.responseText);
-            var html = '';
-
-            $('#commentsCounter').text(comments.length);
-
-            for (var i = 0; i < comments.length; i++) {
-                html += '<div class="box"><div class="content"><div class="box__head"><sub>' + new Date(comments[i].createdDate).toLocaleDateString() +
-                    ' ' + new Date(comments[i].createdDate).toLocaleTimeString() + '</sub><span class="rating"><span class="has-text-success">25</span>' +
-                    '<button class="icon has-text-success" data-attribute="plusForComment"><i class="fa fa-plus-square"></i></button>' +
-                    '<button class="icon has-text-danger" data-attribute="minusForComment"><i class="fa fa-minus-square"></i></button></span>' +
-                    '</div><p class="comment__content">' + comments[i].content + '</p><div class="box__footer">' +
-                    '<button class="button is-primary is-small" data-attribute="reportComment"><i class="fa fa-warning"></i> Report</button>' +
-                    '</div></div></div>';
+                $('#commentsCounter').text(comments.length);
+    
+                for (var i = 0; i < comments.length; i++) {
+                    html += '<div class="box"><div class="content"><div class="box__head"><sub>' + new Date(comments[i].createdDate).toLocaleDateString() +
+                        ' ' + new Date(comments[i].createdDate).toLocaleTimeString() + '</sub><span class="rating"><span class="has-text-success">25</span>' +
+                        '<button class="icon has-text-success" data-attribute="plusForComment"><i class="fa fa-plus-square"></i></button>' +
+                        '<button class="icon has-text-danger" data-attribute="minusForComment"><i class="fa fa-minus-square"></i></button></span>' +
+                        '</div><p class="comment__content">' + comments[i].content + '</p><div class="box__footer">' +
+                        '<button class="button is-primary is-small" data-attribute="reportComment"><i class="fa fa-warning"></i> Report</button>' +
+                        '</div></div></div>';
+                }
+    
+                $('#existing-comments').html(safeResponse.cleanDomString(html));
             }
-
-            $('#existing-comments').html(safeResponse.cleanDomString(html));
-        }
-    };    
-    
-    xhr.open("GET", getCommentsUrl() + "?link=" + url, true);
-    xhr.send();
+        },
+        dataType: "json"
+    });
 }
 
 function sendComment() {
@@ -83,24 +80,22 @@ function fadeOutElement(id) {
 }
 
 function AddCommentAjaxQuery(url) {
-    var xhr = new XMLHttpRequest();
+    
     var commentContent = document.getElementById('comment').value;
     var language = getCurrentLanguage();
     var link = url;
-    var params = "Link=" + link + '&NewCommentContent=' + encodeURIComponent(commentContent) + '&CommentLanguage=' + language;
+    var data = "Link=" + link + '&NewCommentContent=' + encodeURIComponent(commentContent) + '&CommentLanguage=' + language;
 
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState != 4) {
-            return;
-        }
+    $.ajax({
+        type: "POST",
+        url: getAddCommentUrl(),
+        contentType:"application/x-www-form-urlencoded",
+        data: data,
+        success: function() {
 
-        document.getElementById('comment').value = '';
-        GetExistingComments();
-    };    
-
-    xhr.open("POST", getAddCommentUrl(), true);
-
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    xhr.send(params);
+            document.getElementById('comment').value = '';
+            GetExistingComments();
+        },
+        dataType: "json"
+      });
 }
